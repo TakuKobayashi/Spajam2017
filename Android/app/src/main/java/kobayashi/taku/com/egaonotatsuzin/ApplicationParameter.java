14f03780ebd9e88d5d9f47d132e46290d4fe2d0e
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-05-28 08:06:25 +0900

    閾値調整

--- Android/app/src/main/java/kobayashi/taku/com/egaonotatsuzin/ApplicationParameter.java
+++ Android/app/src/main/java/kobayashi/taku/com/egaonotatsuzin/ApplicationParameter.java
@@ -1,6 +1,6 @@
 package kobayashi.taku.com.egaonotatsuzin;
 
 public class ApplicationParameter {
-	public static final float SMILE_THREATHOLD = 0.1f;
+	public static final float SMILE_THREATHOLD = 0.12f;
 	public static final long CIRCLE_REACHED_MILLISECOND = 2000;
 }
