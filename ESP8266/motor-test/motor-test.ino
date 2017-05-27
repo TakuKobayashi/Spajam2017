97b6655c981199fbabc89133142658f44885f642
Erik Hornberger <hornbergererik@gmail.com>
2017-05-27 14:28:59 +0900

    コンパイルしないコードを修正

--- ESP8266/motor-test/motor-test.ino
+++ ESP8266/motor-test/motor-test.ino
@@ -1,13 +1,14 @@
 #include <Stepper.h>
+#include <ESP8266WiFi.h>
 
 const int stepsPerRev = 200; // The number of steps in one revolution for the stepper motor being used
 const int rpm = 10;          // The speed of the motor in RPM
-Stepper   stepper(stepsPerRev,  D5, D6, D7, D8); // Object for controlling the stepper motor. The last four numbers are the pins on the microcontroller.
+Stepper stepper(stepsPerRev,  D5, D6, D7, D8); // Object for controlling the stepper motor. The last four numbers are the pins on the microcontroller.
 
 void setup() {
-  myStepper.setSpeed(motorSpeed);
+  stepper.setSpeed(rpm);
 }
 
 void loop() {
-  myStepper.step(stepsPerRevolution);
+  stepper.step(stepsPerRev);
 }
